c51d1e15be4e96c4867a4b127716b8d436117434
spookyboogy <matt.tolstoy@gmail.com>
2024-01-24 21:18:17 -0600

    1,000 google searches later, the line : "import '/node_modules/lightgallery/lightgallery.umd.js';"

--- leoGallery.js
+++ leoGallery.js
@@ -1,11 +1,13 @@
+import '/node_modules/lightgallery/lightgallery.umd.js';
+
 // Wait for the document to be ready
 $(document).ready(function() {
     // Get the "leos-gallery" container
-    var galleryContainer = $('#leos-gallery');
+    var $galleryContainer = $('#leos-gallery');
     console.log('dom ready')
     // Dynamically fetch images from the "images" directory
     $.ajax({
-        url: 'images/', // Adjust the path accordingly
+        url: 'images', // Adjust the path accordingly
         success: function(data) {
             console.log(data)
             var imageArray = []
@@ -21,12 +23,23 @@ $(document).ready(function() {
 
                 // Create <a> tag with image
                 var imageTag = $('<a href="' + imageUrl + '"><img src="' + imageUrl + '" alt="Image"></a>');
-                
                 // Append the image tag to the gallery container
-                galleryContainer.append(imageTag);
-            
+                $galleryContainer.append(imageTag);
+                
             });
-        console.log(imageArray);
+            // console.log($galleryContainer[0]);
+            
+            // Initialize LightGallery with the dynamically created gallery items
+            lightGallery($galleryContainer[0], {
+                // dynamic: true,
+                thumbnail: true,
+                // dynamicEl: imageArray, // Use the array of gallery items
+                plugins: [lgZoom, lgThumbnail],
+                licenseKey: 'your_license_key',
+                speed: 500,
+                // ... other settings
+            });
+            console.log(imageArray);
         }
     });
 });
