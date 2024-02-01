9afb757cd9b74e9f2e5099e533f417b95acde4f9
spookyboogy <matt.tolstoy@gmail.com>
2024-02-01 13:22:23 -0600

    thumbnail_generator exif read/write finally working, saving before cleaning up comments / refactoring

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from PIL import Image, ExifTags as tags
+from datetime import datetime
 import os
 
 input_path = './images/'
@@ -7,7 +8,7 @@ output_path = './images/.thumbnails/'
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
-for file_name in os.listdir(input_path):
+for file_name in os.listdir(input_path)[:1]:
     if file_name.lower().endswith('.jpg'):
         input_image_path = os.path.join(input_path, file_name)
         # Add suffix to the filename before the extension
@@ -26,6 +27,65 @@ for file_name in os.listdir(input_path):
 
             # Resize the image
             img.thumbnail((desired_width, desired_height))
-            img.save(output_thumbnail_path)
+            
+            # This is a temporary script for accessing improperly
+            # saved manual metadata tags for date_taken done in GIMP, 
+            # should rewrite images with proper DateTimeOriginal exif tags
+            metadata = img.info
+            date_taken = ""
+            try:
+                _keyword = metadata['photoshop'][1028].decode('utf-8')
+                if "Date_taken" in _keyword or "date_taken" in _keyword:
+                    date_taken = eval(_keyword.split("_taken")[1].split('"')[0].split()[1])
+                else:
+                    print(f'No date_taken value found for {input_image_path}')
+            except:
+                print('no photoshop key or irregularly saved metadata tag')
 
-        print(f'Thumbnail created: {output_thumbnail_path}')
+            if date_taken:
+                datetime_original = datetime.strptime(date_taken, "%m/%d/%Y").strftime("%Y:%m:%d 00:00:00")
+                print(f'Date taken : {date_taken}')
+                print(f"Converted: {datetime_original}")
+                
+                # print(f'direct tag access: {img._getexif()[0x9003]}') # will fail if doesn't exist
+                datetime_original_tag_id = 0x9003  # exif Tag ID for DateTimeOriginal, decimal: 36867
+                # datetime_original_tag = datetime_original_tag_id.to_bytes(2, 'big') + b'\x00\x00'
+                # print(f'datetime_original_tag : {datetime_original_tag}')
+                # datetime_original_tag += len(datetime_original).to_bytes(2, 'big')
+                # datetime_original_tag += datetime_original.encode('utf-8')
+                # print(f'datetime_original_tag updated : {datetime_original_tag}')                
+
+                exif_data = img.getexif() or {}
+                
+                if 36867 not in exif_data:
+                    exif_data.update([(36867, datetime_original)])
+                for i in exif_data.keys(): print(i)
+                
+            else:
+                print(f'No date_taken metadata found for {input_image_path}')
+
+            if date_taken and exif_data:
+                # img.save(input_image_path, exif=exif_data)
+                img.save(output_thumbnail_path, exif=exif_data)
+
+        with Image.open(output_thumbnail_path) as thumb: 
+            print(f'{output_thumbnail_path}\
+                  \n\tDateTimeOriginal:\
+                  \n\t\t{thumb.getexif()[0x9003]}')
+
+        with Image.open(input_image_path) as img:
+            exif_data = img.getexif() or {}
+            if 36867 not in exif_data:
+                if date_taken:
+                    exif_data.update([(36867, datetime_original)])
+            print(f"exif_data[36867] : {exif_data[36867]}")
+            img.save(input_image_path, exif=exif_data)
+        
+        with Image.open(input_image_path) as img: 
+            print(f'{input_image_path}\
+                    \n\tDateTimeOriginal:\
+                    \n\t\t{img.getexif()[0x9003]}')
+            
+        print(f'\n\nThumbnail created: {output_thumbnail_path}')
+
+
