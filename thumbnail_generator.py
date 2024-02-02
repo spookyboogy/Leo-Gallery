005fb98725408c39a6a9ba095a7a21777338672e
spookyboogy <matt.tolstoy@gmail.com>
2024-02-01 18:44:10 -0600

    Now writes manually tagged GIMP keyword to ImageDescription (doesn't overwrite if already exists), Copies existing exif_data to thumbnail, including DateTimeOriginal and ImageDescription if date_taken is not None or if exif tags already exist. In the case that the GIMP keyword is found, the original image's tags are checked and if not found, is updated/overwritten. This unfortunately erases other non-exif metadata.

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -5,10 +5,13 @@ import os
 input_path = './images/'
 output_path = './images/.thumbnails/'
 
+datetime_original_tag_id = 0x9003  # exif Tag ID for DateTimeOriginal, decimal: 36867
+image_description_tag_id = 0x010e  # exif Tag ID for ImageDescription, decimal: 270
+
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
-for file_name in os.listdir(input_path)[:1]:
+for file_name in os.listdir(input_path)[:7]:
     if file_name.lower().endswith('.jpg'):
         input_image_path = os.path.join(input_path, file_name)
         # Add suffix to the filename before the extension
@@ -28,64 +31,84 @@ for file_name in os.listdir(input_path)[:1]:
             # Resize the image
             img.thumbnail((desired_width, desired_height))
             
+            print('_' * 20 + '\n')
+            print(f'Current image : {input_image_path}')
             # This is a temporary script for accessing improperly
             # saved manual metadata tags for date_taken done in GIMP, 
-            # should rewrite images with proper DateTimeOriginal exif tags
+            # Proper DateTimeOriginal exif tags are written to the original 
+            # if the GIMP keyword is found this overwrites the original keyword string, 
+            # but the string is saved in the ImageDescription exif tag. 
+            # If ImageDescription already exists, it's not overwritten.
             metadata = img.info
             date_taken = ""
+            exif_data = img.getexif() or {}
             try:
                 _keyword = metadata['photoshop'][1028].decode('utf-8')
                 if "Date_taken" in _keyword or "date_taken" in _keyword:
                     date_taken = eval(_keyword.split("_taken")[1].split('"')[0].split()[1])
                 else:
-                    print(f'No date_taken value found for {input_image_path}')
+                    print(f'No date_taken value found in gimp metadata.')
             except:
-                print('no photoshop key or irregularly saved metadata tag')
+                # print('\n\tNo metadata[photoshop] key or irregularly saved GIMP metadata tag')
+                # No metadata[photoshop] key or irregularly saved GIMP metadata tag
+                pass
 
             if date_taken:
+                print("""Found manually added GIMP metadata keyword in format "Date_taken : '%m/%d/%y'" """)
                 datetime_original = datetime.strptime(date_taken, "%m/%d/%Y").strftime("%Y:%m:%d 00:00:00")
-                print(f'Date taken : {date_taken}')
-                print(f"Converted: {datetime_original}")
+                print(f"\tDate taken : {date_taken}")
+                print(f"\tConverted  : {datetime_original}")
                 
-                # print(f'direct tag access: {img._getexif()[0x9003]}') # will fail if doesn't exist
-                datetime_original_tag_id = 0x9003  # exif Tag ID for DateTimeOriginal, decimal: 36867
-                # datetime_original_tag = datetime_original_tag_id.to_bytes(2, 'big') + b'\x00\x00'
-                # print(f'datetime_original_tag : {datetime_original_tag}')
-                # datetime_original_tag += len(datetime_original).to_bytes(2, 'big')
-                # datetime_original_tag += datetime_original.encode('utf-8')
-                # print(f'datetime_original_tag updated : {datetime_original_tag}')                
+                if datetime_original_tag_id not in exif_data:
+                    exif_data.update([(datetime_original_tag_id, datetime_original)])
+                if image_description_tag_id not in exif_data:
+                    exif_data.update([(image_description_tag_id, date_taken)])
 
+            img.save(output_thumbnail_path, exif=exif_data)
+            print(f'\nThumbnail created: {output_thumbnail_path}')
+
+        if date_taken:
+            # Add DateTimeOriginal exif tag to original image if it doesn't exist
+            # Add ImageDescription exif tag with original/manual date string if it doesn't exist
+            with Image.open(input_image_path) as img:
                 exif_data = img.getexif() or {}
-                
-                if 36867 not in exif_data:
-                    exif_data.update([(36867, datetime_original)])
-                for i in exif_data.keys(): print(i)
-                
-            else:
-                print(f'No date_taken metadata found for {input_image_path}')
-
-            if date_taken and exif_data:
-                # img.save(input_image_path, exif=exif_data)
-                img.save(output_thumbnail_path, exif=exif_data)
-
-        with Image.open(output_thumbnail_path) as thumb: 
-            print(f'{output_thumbnail_path}\
-                  \n\tDateTimeOriginal:\
-                  \n\t\t{thumb.getexif()[0x9003]}')
-
-        with Image.open(input_image_path) as img:
-            exif_data = img.getexif() or {}
-            if 36867 not in exif_data:
-                if date_taken:
-                    exif_data.update([(36867, datetime_original)])
-            print(f"exif_data[36867] : {exif_data[36867]}")
-            img.save(input_image_path, exif=exif_data)
+                # Only update and call img.save() on OG image if relevant tags don't already exist
+                # The downside of editing exif data with PIL is that there is no way to preserve
+                # the entire metadata of a file when saving 'in place'. This could possibly be overcome
+                # by using temp copies or something but cba, this is written for very specific usage at the moment.
+                overwriting_metadata = False
+                if datetime_original_tag_id not in exif_data:
+                    exif_data.update([(datetime_original_tag_id, datetime_original)])
+                    overwriting_metadata = True
+                if image_description_tag_id not in exif_data:
+                    exif_data.update([(image_description_tag_id, date_taken)])
+                    overwriting_metadata = True
+                if overwriting_metadata:
+                    img.save(input_image_path, exif=exif_data)
         
-        with Image.open(input_image_path) as img: 
-            print(f'{input_image_path}\
-                    \n\tDateTimeOriginal:\
-                    \n\t\t{img.getexif()[0x9003]}')
-            
-        print(f'\n\nThumbnail created: {output_thumbnail_path}')
+        print("\nChecking exif tags post processing.")
+        # Check that exif tag saved correctly for both
+        test_paths = [input_image_path, output_thumbnail_path]
+        test_tags = [datetime_original_tag_id, image_description_tag_id]
+        for test_path in test_paths:
+            print(f'\t{test_path}')
+            with Image.open(test_path) as img: 
+                for tag in test_tags:
+                    try: 
+                        print(f'\t\ttag : {tag}:\n\t\t\t{img.getexif()[tag]}')
+                    except:
+                        print(f'\t\ttag : {tag}:\n\t\t\t Not found')
+        print('_' * 20 )
+        
 
 
+# # this is one way of viewing the GIMP keyword that I added manually with the format "Date_taken : '%M/%D/%Y'"
+# # there is however no way (to my knowledge) to write non-exif metadata using Image.save(out.txt, exif=exif_data)
+# 
+# with Image.open(input_image_path) as img:
+#     img.info
+#     for i in img.metadata['photoshop']: print(i)
+#     print(img.app['APP1'])
+#
+# # The other method, also unwriteable afaik 
+# _keyword = img.info['photoshop'][1028].decode('utf-8')
