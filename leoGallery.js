8c7753cb22ac230e1cfa1cbd56d12876d9bc1d90
spookyboogy <matt.tolstoy@gmail.com>
2024-02-02 11:30:21 -0600

    implemented /images/images.json importing, adding to leosGallery from array, array is sorted by DateTimeOriginal before adding html tags, removed old comments/code

--- leoGallery.js
+++ leoGallery.js
@@ -5,32 +5,68 @@ import '/node_modules/lightgallery/plugins/autoplay/lg-autoplay.umd.js';
 import '/node_modules/lightgallery/plugins/rotate/lg-rotate.umd.js';
 import '/node_modules/lightgallery/plugins/zoom/lg-zoom.umd.js';
 import '/node_modules/justifiedGallery/dist/js/jquery.justifiedGallery.min.js';
+import images from '/images/images.json' assert { type: 'json' };
 
 const $galleryContainer = $('#leos-gallery');
+let imageArray = images.images;
 
-// Wait for the document to be ready
+// Custom compare function
+function compareByDateTimeOriginal(a, b) {
+  const parseDate = (dateString) => {
+    if (!dateString) {
+      return null; // Return null for items without DateTimeOriginal
+    }
+    const [datePart, timePart] = dateString.split(' ');
+    const [year, month, day] = datePart.split(':');
+    const [hours, minutes, seconds] = timePart.split(':');
+    return new Date(year, month - 1, day, hours, minutes, seconds);
+  };
+
+  const dateTimeA = parseDate(a.exif.DateTimeOriginal);
+  const dateTimeB = parseDate(b.exif.DateTimeOriginal);
+
+  // Handle cases where one or both objects lack DateTimeOriginal
+  if (dateTimeA === null && dateTimeB === null) {
+    return 0; // Both items are considered equal
+  }
+  if (dateTimeA === null) {
+    return 1; // Place items without DateTimeOriginal at the end
+  }
+  if (dateTimeB === null) {
+    return -1; // Place items without DateTimeOriginal at the end
+  }
+
+  // Compare dates
+  if (dateTimeA < dateTimeB) {
+    return -1;
+  }
+  if (dateTimeA > dateTimeB) {
+    return 1;
+  }
+  return 0;
+}
+
+imageArray.sort(compareByDateTimeOriginal);
+// console.log(imageArray);
+
+for (var i = 0; i < imageArray.length; i++) {
+  var image = imageArray[i];
+  var imageUrl = `/images/${image.filename}`;
+  var thumbnailUrl = image.thumbnail;
+  var dateTimeOriginal = image.exif.DateTimeOriginal;
+  var imageDescription = image.exif.ImageDescription;
+  // Create <a> tag with image
+  var imageTag = $(
+    '<a href="' + imageUrl + '">' + 
+      '<img src="' + thumbnailUrl + '" alt="' + imageDescription + '">' +
+    '</a>');
+  // Append the image tag to the gallery container
+  $galleryContainer.append(imageTag);
+};
+
+// Wait for the document to be ready, initialize LightGallery and JustifiedGallery
 $(function() {
   console.log('dom ready');
-  
-  // Hardcoding relative image URLs based on the pattern Leo_xx.jpg 
-  var imageArray = [];
-  for (var i = 3; i <= 93; i++) {
-    var imageNumber = i.toString().padStart(2, '0'); // Add leading zeros if needed
-    var imageUrl = '/images/Leo_' + imageNumber + '.jpg';
-    var thumbnailUrl = '/images/.thumbnails/Leo_' + imageNumber + '_small.jpg';
-
-    // Add image information to the array
-    imageArray.push({
-      src: imageUrl,
-      thumb: thumbnailUrl,
-      subHtml: 'Image Caption ' + (i + 1)
-    });
-
-    // Create <a> tag with image
-    var imageTag = $('<a href="' + imageUrl + '"><img src="' + thumbnailUrl + '" alt="Image"></a>');
-    // Append the image tag to the gallery container
-    $galleryContainer.append(imageTag);
-  }
   
   // Initialize LightGallery with the dynamically created gallery items
   lightGallery($galleryContainer[0], {
@@ -38,8 +74,6 @@ $(function() {
     plugins: [lgZoom, lgThumbnail, lgAutoplay, lgFullscreen, lgRotate],
     licenseKey: 'your_license_key',
     speed: 500,
-    // dynamic: true,
-    // dynamicEl: imageArray,
     // ... other settings
   });
   // Initialize justifiedGallery 
@@ -54,7 +88,6 @@ $(function() {
     margins: 4
   }); 
 });
-
 
 // Chrome gives the following warning
 // leoGallery.js:32 (calling lightGallery) [Violation] Added non-passive 
@@ -78,24 +111,5 @@ $(function() {
 //   window.removeEventListener("testPassive", null, opts);
 // } catch (e) {}
 
-// // Use our detect's results. passive applied if supported, capture will be false either way.
+// // Use the detect's results. passive applied if supported, capture will be false either way.
 // $galleryContainer[0].addEventListener('touchstart', function() {}, supportsPassive ? { passive: true } : false);
-
-
-// // For when using a .json to keep a list of images in /images
-//
-// import images from './images.json'
-// images.forEach(function(image) {
-//     var imageUrl = image.url;
-//
-//     imageArray.push({
-//         src: imageUrl,
-//         thumb: imageUrl,
-//         subHtml: 'Image Caption'
-//     });
-//     // Create <a> tag with image
-//     var imageTag = $('<a href="' + imageUrl + '"><img src="' + imageUrl + '" alt="Image"></a>');
-//     // Append the image tag to the gallery container
-//     $galleryContainer.append(imageTag);
-// });
-
